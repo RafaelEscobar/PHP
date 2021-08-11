56484caf0f59b3a28b00dff69a427755f0ae46c2
RafaelEscobar <rafael031015@gmail.com>
2021-08-11 16:47:24 -0500

    Borrar carpeta

--- base datos/prueba.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('Connect.php');
-require_once('../POO/User.php');
-
-$connect = new Connect();
-
-$user = new User(random_int(1, 100));
-$user->validar("rafael031015@gmail.com", )
-
